abe30a212b21c5f8eeb917bd3c2d68946aa4ac1e
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-06 16:22:13 +0300

    traversal

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,50 +1,22 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - function that inserts to the
- * left of a binary treee
- *
- * @parent: the node at which a new node will
- * be inserted at the left
- * @value: the vale to be inserted
- *
- * Return: a pointer to the new node
- */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+ * binary_tree_insert_left - function to insert a node as a left-child of another node
+ * @parent: Pointer to the node node to insert in
+ * @value: value to store in the new node
+ * Return: Pointer to the created node or NUll
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
+binary_tree_t *new_node = binary_tree_node(parent, value);
+if (parent == NULL)
+return (NULL);
+if (new_node == NULL)
+return (NULL);
+if (parent->left != NULL)
 {
-	binary_tree_t *Newnode;
-
-	/*check if parent is empty*/
-	if (parent == NULL)
-		return (NULL);
-
-	/**
-	 * first create a new node by utilizing our
-	 * previous newnode function
-	 */
-	Newnode = binary_tree_node(parent, value);
-
-	/*check if the newnode creation was successful*/
-	if (Newnode == NULL)
-		return (NULL);
-
-	/*check if the parent node already has a left child*/
-	if (parent->left != NULL)
-	{
-		/**
-		 * sets the newnode left pointer to the current
-		 * left child of the parent
-		 */
-		Newnode->left = parent->left;
-
-		/**
-		 * Updates the current left child's parent pointer
-		 * to the new node
-		*/
-		parent->left->parent = Newnode;
-	}
-	/*else Sets the parent's left pointer to the new node*/
-	parent->left = Newnode;
-
-	/*return pointer to the created node*/
-	return (Newnode);
+parent->left->parent = new_node;
+new_node->left = parent->left;
+}
+parent->left = new_node;
+return (new_node);
 }
---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - traverse a tree using postorder
+ * @tree: tree to traverse
+ * @func: a pointer to a func to call for each node
+ * The value in the node must be passed as a parameter in func
+ * Return: Nothing if tree or func is NULL
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)){
+if (tree == NULL || func == NULL)
+return;
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
